8a7419b208a2da28619712d0d5529d7ff2722257
unknown <adrianasoto1417@gmail.com>
2021-11-11 12:06:24 -0500

    prueba de ruteo

--- burger/src/app/app.module.ts
+++ burger/src/app/app.module.ts
@@ -3,10 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PruebaComponent } from './componentes/prueba/prueba/prueba.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PruebaComponent
   ],
   imports: [
     BrowserModule,
