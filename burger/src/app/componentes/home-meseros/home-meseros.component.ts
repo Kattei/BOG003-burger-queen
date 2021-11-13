238be368d2f164a11c8795f4575b12d23af191fb
leidy acevedo <leidyjuliethaj@ufps.edu.co>
2021-11-13 16:25:00 -0500

    visualizacion de data de .json en consola

--- burger/src/app/componentes/home-meseros/home-meseros.component.ts
+++ burger/src/app/componentes/home-meseros/home-meseros.component.ts
@@ -1,18 +1,19 @@
 import { Component, OnInit } from '@angular/core';
 
+
 @Component({
   selector: 'app-home-meseros',
   templateUrl: './home-meseros.component.html',
   styleUrls: ['./home-meseros.component.scss']
 })
 export class HomeMeserosComponent implements OnInit {
-  showBreakfast:boolean =false;
+  showBreakfast:boolean = false;
 
   constructor() { }
 
   ngOnInit(): void {
   }
-
+  
 
   getMenuDesayuno():void{
 
--- burger/src/app/componentes/home-meseros/subcomponentes-mesero/menu-desayuno/menu-desayuno.component.ts
+++ burger/src/app/componentes/home-meseros/subcomponentes-mesero/menu-desayuno/menu-desayuno.component.ts
@@ -1,11 +1,33 @@
 import { Component, OnInit } from '@angular/core';
 
+
+
 @Component({
   selector: 'app-menu-desayuno',
   templateUrl: './menu-desayuno.component.html',
   styleUrls: ['./menu-desayuno.component.scss']
 })
 export class MenuDesayunoComponent implements OnInit {
+  items=[];
+  desayun:any = [];
+  visualizarData = () => {
+    fetch("./assets/menus.json")
+    .then(msg=>msg.json())
+    .then(data=> console.log(data))
+
+    // .then(data=>{
+      
+    //   // this.desayun=data;
+    //   this.desayun=data;
+    //   //return this.desayun
+    //   // console.log(this.desayun.desayuno[0])
+    //   console.log('oka',this.desayun)
+    
+    // })
+    
+  }
+  
+  
 
   constructor() { }
 
