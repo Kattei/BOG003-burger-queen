b62894ab9c9d90661a54bec36e55a358a8742a5e
Kattei <86080736+Kattei@users.noreply.github.com>
2021-11-17 13:17:48 -0500

    modificacion  en el componente inc-dec y en le json

--- burger/src/app/componentes/home-meseros/subcomponentes-mesero/inc-dec/inc-dec.component.ts
+++ burger/src/app/componentes/home-meseros/subcomponentes-mesero/inc-dec/inc-dec.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
 
+import { Item } from 'src/app/clases/item';
+
 
 
 @Component({
@@ -8,16 +10,21 @@ import { Component, OnInit, Input } from '@angular/core';
   styleUrls: ['./inc-dec.component.scss']
 })
 export class IncDecComponent implements OnInit {
-incremento:number = 1
-number:number=0
-@Input() itemP: string | undefined
+//incremento:number = 1
+//number:number=0
+
+@Input()  itemP: Item | undefined
   constructor() { }
 
   ngOnInit(): void {
     console.log(this.itemP)
   }
 contador = (incremento:number) =>{
-  this.number += incremento
+  if(this.itemP !==undefined){
+
+    this.itemP.cantidad += incremento
+  }
+console.log(this.itemP);
 
 }
 
